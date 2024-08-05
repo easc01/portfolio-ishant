67f41fdc74bce74fc08de442a420aa51ae8bf24c
ishantSikdar <ishant9715@gmail.com>
2024-08-06 03:23:36 +0530

    Added Resume btn

--- src/components/Email.tsx
+++ src/components/Email.tsx
@@ -1,11 +1,20 @@
+import { FileTextIcon } from "@radix-ui/react-icons";
 import { MailIcon } from "./Icons";
 
 export const Email = () => (
-  <div className="flex items-center z-50 bg-pitch-black/50 backdrop-blur-md fixed bottom-5 right-5 rounded-full border-2 border-brown-white p-1">
-    <div className="bg-white w-max m-auto rounded-full p-1 lg:p-2">
-      <MailIcon className="text-black" />
+  <div className="fixed bottom-5 right-5 flex flex-col items-end gap-3 z-50">
+    <div className="flex w-max items-center bg-pitch-black/50 backdrop-blur-md  rounded-full border-2 border-brown-white p-1">
+      <div className="bg-white w-max m-auto rounded-full p-1 lg:p-2">
+        <FileTextIcon className="text-black" />
+      </div>
+      <a href="/resume.pdf" download="resume-ishant-sikdar.pdf" className="px-2 text-xs lg:text-base">Resume</a>
     </div>
 
-    <a href="mailto:ishant9715@gmail.com" className="px-2 text-xs lg:text-base">ishant9715@gmail.com</a>
+    <div className="flex items-center bg-pitch-black/50 backdrop-blur-md  rounded-full border-2 border-brown-white p-1">
+      <div className="bg-white w-max m-auto rounded-full p-1 lg:p-2">
+        <MailIcon className="text-black" />
+      </div>
+      <a href="mailto:ishant9715@gmail.com" className="px-2 text-xs lg:text-base">ishant9715@gmail.com</a>
+    </div>
   </div>
 )

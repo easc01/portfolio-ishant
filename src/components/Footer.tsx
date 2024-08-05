aee5521df1c95d653700fde80ff1583290155623
ishantSikdar <ishant9715@gmail.com>
2024-08-06 02:38:57 +0530

    Responsive UI

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,40 +6,40 @@ export default function Footer() {
   const [homeRef, experienceRef, projectRef] = useScrollContext();
 
   return <div>
-    <div className="p-10 mx-4 mb-4 flex justify-between font-poppins border border-warm-white/20 rounded-xl">
-      <div className="w-1/4">
-        <p className="text-2xl">
+    <div className="p-5 md:p-10 mx-4 mb-4 flex gap-5 flex-col md:flex-row justify-between font-poppins border border-warm-white/20 rounded-xl">
+      <div className="md:w-1/4">
+        <p className="text-sm md:text-2xl">
           Delivering <span className="text-cyan-500">Precision</span> and <span className="text-purple-500">Performance</span> in Tech
         </p>
       </div>
 
-      <div className="w-1/4 h-full">
-        <p className="pb-5 text-orange-300 font-medium">Explore More</p>
+      <div className="md:w-1/4 h-full">
+        <p className="md:pb-5 text-sm md:text-base text-orange-300 font-medium">Explore More</p>
 
-        <div className="flex items-start flex-col gap-2">
+        <div className="flex items-start md:flex-col gap-2">
           <button onClick={() => scrollToView(homeRef)} className="hover:text-blue-500 transition-colors duration-200 ease-in-out">Home</button>
           <button onClick={() => scrollToView(experienceRef)} className="hover:text-blue-500 transition-colors duration-200 ease-in-out">Experience</button>
           <button onClick={() => scrollToView(projectRef)} className="hover:text-blue-500 transition-colors duration-200 ease-in-out">Projects</button>
         </div>
       </div>
 
-      <div className="w-1/4 h-full">
-        <p className="pb-5 text-green-500 font-medium">Socials</p>
+      <div className="md:w-1/4 h-full">
+        <p className="pb-2 md:pb-5 text-sm md:text-base text-green-500 font-medium">Socials</p>
 
-        <div className="flex items-start flex-col text-sm gap-3">
-          <a href="https://www.linkedin.com/in/ishantsikdar" className="flex gap-3 items-center">
+        <div className="flex items-start flex-wrap md:flex-col text-sm gap-2 md:gap-3">
+          <a href="https://www.linkedin.com/in/ishantsikdar" className="flex gap-1 md:gap-3 items-center">
             <LinkedInIcon className="size-5" />
             <p className="hover:text-blue-500 transition-colors duration-200 ease-in-out">LinkedIn</p>
           </a>
-          <a href="https://github.com/ishantSikdar" className="flex gap-3 items-center">
+          <a href="https://github.com/ishantSikdar" className="flex gap-1 md:gap-3 items-center">
             <GitHubIcon className="size-5" />
             <p className="hover:text-blue-500 transition-colors duration-200 ease-in-out">GitHub</p>
           </a>
-          <a href="https://leetcode.com/u/eaSc01" className="flex gap-3 items-center">
+          <a href="https://leetcode.com/u/eaSc01" className="flex gap-1 md:gap-3 items-center">
             <LeetCodeIcon className="size-5" />
             <p className="hover:text-blue-500 transition-colors duration-200 ease-in-out">LeetCode</p>
           </a>
-          <a href="https://x.com/Ishant_Sikdar" className="flex gap-3 items-center">
+          <a href="https://x.com/Ishant_Sikdar" className="flex gap-1 md:gap-3 items-center">
             <TwitterIcon className="size-5" />
             <p className="hover:text-blue-500 transition-colors duration-200 ease-in-out">Twitter</p>
           </a>

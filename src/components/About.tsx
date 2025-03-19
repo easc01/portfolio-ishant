e7deb361e0c07d7ffb45aa9ebfe0a8dcdefbb3a6
Ishant S. <ishant9715@gmail.com>
2025-03-19 10:46:12 +0530

    changes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,17 +1,34 @@
 export default function About() {
-  return <div className="flex flex-col gap-4 md:gap-10 text-white  py-16">
-    <div className="text-left md:w-[55%]">
-      <h2 className="md:text-3xl">I'm a <span className="text-purple-500">Software Engineering</span> Student,</h2>
-      <h2 className="text-xl md:text-4xl">Specialising in develop and deploying <span className="text-orange-300">scalable web applications</span>.</h2>
-    </div>
+  return (
+    <div className="flex flex-col gap-4 md:gap-10 text-white  py-16">
+      <div className="text-left md:w-[55%]">
+        <h2 className="text-xl md:text-4xl">
+          I specialize in developing and deploying{" "}
+          <span className="text-orange-300">scalable web applications</span>.
+        </h2>
+      </div>
 
-    <div className="w-[80%] h-[1px] mx-auto bg-white"></div>
+      <div className="w-[80%] h-[1px] mx-auto bg-white"></div>
 
-    <div className="md:text-right  flex flex-col items-end">
-      <div className="md:w-[70%]">
-        <h2 className="md:text-3xl">I expertise in building apps on <span className="px-2 bg-white font-bold text-pitch-black rounded-md">Java</span> and <span className="px-2 bg-white font-bold text-pitch-black rounded-md">TypeScript</span></h2>
-        <h2 className="text-xl md:text-4xl">Currently working on <span className="text-cyan-400">React.js</span>, <span className="text-orange-300">Nest.js</span> <br/> and <span className="text-purple-500">Cloud Computing</span>.</h2>
+      <div className="md:text-right  flex flex-col items-end">
+        <div className="md:w-[70%]">
+          <h2 className="md:text-3xl">
+            I expertise in building apps on{" "}
+            <span className="px-2 bg-white font-bold text-pitch-black rounded-md">
+              Java
+            </span>{" "}
+            and{" "}
+            <span className="px-2 bg-white font-bold text-pitch-black rounded-md">
+              TypeScript
+            </span>
+          </h2>
+          <h2 className="text-xl md:text-4xl">
+            Currently working on <span className="text-cyan-400">React.js</span>
+            , <span className="text-orange-300">Nest.js</span> <br /> and{" "}
+            <span className="text-purple-500">Cloud Computing</span>.
+          </h2>
+        </div>
       </div>
     </div>
-  </div>
+  );
 }
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,28 +3,34 @@ import HomeRef from "./HomeRef";
 import SocialsDock from "./SocialsDock";
 
 export default function HeroSection() {
-  return <>
-    <HomeRef />
-    <div className="h-[100dvh] relative flex w-full flex-col items-center justify-center">
-      <span className="md:text-left flex flex-col items-center gap-2 z-30 ">
-        <h2 className="p-2  border-white w-max">Hello, I'm Ishant, a</h2>
-        <div className="font-obitron uppercase font-extrabold text-center tracking-widest text-4xl md:text-5xl lg:text-6xl">
-          <h1 className="text-warm-white">Fullstack</h1>
-          <h1 className="text-warm-white">Developer</h1>
-        </div>
-        <h2 className="text-xs lg:text-sm rounded-full mt-2 p-2 border-2 border-brown-white text-center">I build dynamic web <span className="text-orange-300">applications</span> from concept to <span className="text-cyan-400">deployment</span></h2>
-        <div className="absolute bottom-10">
-          <SocialsDock />
-        </div>
-      </span>
+  return (
+    <>
+      <HomeRef />
+      <div className="h-[100dvh] relative flex w-full flex-col items-center justify-center">
+        <span className="md:text-left flex flex-col items-center gap-2 z-30 ">
+          <h2 className="p-2  border-white w-max">Hello, I'm Ishant, a</h2>
+          <div className="font-obitron uppercase font-extrabold text-center tracking-widest text-4xl md:text-5xl lg:text-6xl">
+            <h1 className="text-warm-white">Software</h1>
+            <h1 className="text-warm-white">Engineer</h1>
+          </div>
+          <h2 className="text-xs lg:text-sm rounded-full mt-2 p-2 border-2 border-brown-white text-center">
+            I build dynamic web{" "}
+            <span className="text-orange-300">applications</span> from concept
+            to <span className="text-cyan-400">deployment</span>
+          </h2>
+          <div className="absolute bottom-10">
+            <SocialsDock />
+          </div>
+        </span>
 
-      <Particles
-        className="absolute inset-0"
-        quantity={300}
-        ease={80}
-        color={'#ffffff'}
-        refresh
-      />
-    </div>
-  </>
-};
+        <Particles
+          className="absolute inset-0"
+          quantity={300}
+          ease={80}
+          color={"#ffffff"}
+          refresh
+        />
+      </div>
+    </>
+  );
+}

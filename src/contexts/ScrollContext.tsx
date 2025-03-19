59d8a0628ba3354a0396c7b50a56d53d8affe3e9
Ishant S. <ishant9715@gmail.com>
2025-03-19 10:31:35 +0530

    feat: smooth scrolling

--- src/contexts/ScrollContext.tsx
+++ src/contexts/ScrollContext.tsx
@@ -1,15 +1,39 @@
-'use client'
+"use client";
 
-import React, { createContext, useContext, useRef, ReactNode } from 'react';
+import Lenis from "@studio-freight/lenis";
+import React, {
+  createContext,
+  useContext,
+  useRef,
+  ReactNode,
+  useEffect,
+} from "react";
 
-type ScrollContextType = [React.RefObject<HTMLDivElement>, React.RefObject<HTMLDivElement>, React.RefObject<HTMLDivElement>];
+type ScrollContextType = [
+  React.RefObject<HTMLDivElement>,
+  React.RefObject<HTMLDivElement>,
+  React.RefObject<HTMLDivElement>
+];
 
 const ScrollContext = createContext<ScrollContextType | null>(null);
 
-export const ScrollProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+export const ScrollProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
   const homeRef = useRef<HTMLDivElement>(null);
   const experienceRef = useRef<HTMLDivElement>(null);
   const projectRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const lenis = new Lenis();
+    const raf = (time: number) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+    requestAnimationFrame(raf);
+
+    return () => lenis.destroy();
+  }, []);
 
   return (
     <ScrollContext.Provider value={[homeRef, experienceRef, projectRef]}>
@@ -21,7 +45,7 @@ export const ScrollProvider: React.FC<{ children: ReactNode }> = ({ children }) 
 export const useScrollContext = () => {
   const context = useContext(ScrollContext);
   if (context === null) {
-    throw new Error('useScrollContext must be used within a ScrollProvider');
+    throw new Error("useScrollContext must be used within a ScrollProvider");
   }
   return context;
 };
